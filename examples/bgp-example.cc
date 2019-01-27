892d92487e495bfd786c2666f62ee230a2b6124a
Honghao Zeng <nat@nat.moe>
2019-01-27 02:29:23 -0500

    example update.

--- examples/bgp-example.cc
+++ examples/bgp-example.cc
@@ -14,14 +14,16 @@ using namespace ns3;
  *                   /
  *  AS65000 - AS65001 
  *                   \
- *                    AS65003
+ *                    AS65003 - AS65004
+ * AS65000: 172.16.0.0/24
+ * AS65004: 172.20.0.0/24
  */
 
 int main () {
     LogComponentEnable ("BGPSpeaker", LOG_LEVEL_ALL);
 
     NodeContainer n;
-    n.Create(4);
+    n.Create(5);
     
     InternetStackHelper internet;
     internet.Install(n);
@@ -37,8 +39,10 @@ int main () {
     BGPHelper bgp1 (65001);
     BGPHelper bgp2 (65002);
     BGPHelper bgp3 (65003);
+    BGPHelper bgp4 (65004);
 
-    bgp0.AddRoute(Ipv4Address("172.16.0.0"), 24); // route: 172.16.0.0/24
+    bgp0.AddRoute(Ipv4Address("172.16.0.0"), 24); // r0 route: 172.16.0.0/24
+    bgp4.AddRoute(Ipv4Address("172.20.0.0"), 24); // r5 route: 172.20.0.0/24
 
     bgp0.AddPeer(i.GetAddress(1), 65001);
     bgp1.AddPeer(i.GetAddress(0), 65000);
@@ -46,22 +50,26 @@ int main () {
     bgp1.AddPeer(i.GetAddress(2), 65002);
     bgp2.AddPeer(i.GetAddress(1), 65001);
 
-    bgp2.AddPeer(i.GetAddress(3), 65003);
-    bgp3.AddPeer(i.GetAddress(2), 65002);
+    bgp1.AddPeer(i.GetAddress(3), 65003);
+    bgp3.AddPeer(i.GetAddress(1), 65001);
+
+    bgp3.AddPeer(i.GetAddress(4), 65004);
+    bgp4.AddPeer(i.GetAddress(3), 65003);
 
     auto a0 = bgp0.Install(n.Get(0));
     auto a1 = bgp1.Install(n.Get(1));
     auto a2 = bgp2.Install(n.Get(2));
     auto a3 = bgp3.Install(n.Get(3));
+    auto a4 = bgp4.Install(n.Get(4));
 
     a0.Start(Seconds(0.0));
     a1.Start(Seconds(0.0));
     a2.Start(Seconds(0.0));
     a3.Start(Seconds(0.0));
+    a4.Start(Seconds(0.0));
 
     Simulator::Run();
 
     return 0;
-
 
 }
